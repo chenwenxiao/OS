b9c21428eb93cbe994476f6d670b1b5486496a33
chenwenxiao <chenwenxiao@live.cn>
2016-03-09 18:07:03 +0800

    readme

--- hw6/virtual.cpp
+++ hw6/virtual.cpp
@@ -31,13 +31,17 @@ int main() {
 	int psy_index = address & Q;
 	pte = page[pde][pde_index];
 	cout << hex << "pde index: " << pde_index << " pde contents: " << pte << " valid:" << (pte >> 7) << " pfn:" << (pte & L) << endl;
-	if ((pte >> 7) == 0)
+	if ((pte >> 7) == 0) {
 		cout << "Fault (page directory entry not valid)" << endl;
+		return ;
+	}
 	pte = pte & L;
 	psy = page[pte][pte_index];
 	cout << hex << "pte index: " << pte_index << " contents: " << psy << " valid:" << (psy >> 7) << " pfn:" << (psy & L) << endl;
-	if ((psy >> 7) == 0)
+	if ((psy >> 7) == 0) {
 		cout << "Fault (page table entry not valid)" << endl;
+		return ;
+	}
 	psy = psy & L;
 	cout << hex << "translate : " << (psy << 5) + psy_index << " contents: " << page[psy][psy_index] << endl;
 	
